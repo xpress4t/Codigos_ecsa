cf7157120f443fa5a87ab172c8fb9718f1644f28
xpress4t <emmanuelcm2899@gmail.com>
2024-05-13 14:00:03 +0200

    1321231

--- 3er Trimestre/Temas de Python/99.-Pruebas/paraDaichi.py
+++ 3er Trimestre/Temas de Python/99.-Pruebas/paraDaichi.py
@@ -1,10 +1,9 @@
 import pyautogui
 import time
 import webbrowser
-
 webbrowser.open("https://chat.whatsapp.com/EEedN8CUurOHI8npKKVHfK")
 time.sleep(5)
 
 for i in range(100):
-    pyautogui.write("Daichi maricon")
+    pyautogui.write("@Daichi 2  Daichi maricon")
     pyautogui.press('enter')    
